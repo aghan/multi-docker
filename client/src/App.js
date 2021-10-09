f8c442306da8cb35b0d86670920e2b29027c9942
Aditya Ghan <adityag@appnovation.com>
2021-10-09 17:56:46 +0530

    Quick change to see redeployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fib Calculator - Aditya Ghan
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
